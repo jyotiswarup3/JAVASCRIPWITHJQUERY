ac4686f88a19930401836465a23209927da8fc8d
Jyotiswarup <jyotijohn305@gmail.com>
2024-03-07 11:19:18 +0530

    added multiple files and completed game yet to complete the hint part

--- game.js
+++ game.js
@@ -1,50 +1,118 @@
-var buttonColours=[
-    "red", "blue", "green", "yellow"]
-var gamePattern=[];
+var buttonColours = ['red', 'blue', 'green', 'yellow'];
+var gamePattern = [];
 var userClickedPattern = [];
-var level=0;
+var level = 0;
 var started = false;
+var high = false;
+var highscore = 0;
 
-$(document).keypress(function() {
-    if (!started) {
-    $("#level-title").text("Level " + level);
-      nextSequence();
-      started = true;
-    }
-  });
+$(document).keypress(function () {
+  if (!started) {
+    $('#level-title').text('Level ' + level);
+    nextSequence();
+    started = true;
+  }
+});
 
-function nextSequence(){       
-var randomno=Math.floor(Math.random()*4);
-var randomChosenColour=buttonColours[randomno];
-gamePattern.push(randomChosenColour);
-$("#"+randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-playSound(randomChosenColour);
-$("#level-title").text("Level "+level);
-level++;
+function nextSequence() {
+  highScore();
+  level++;
+  var randomno = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomno];
+  gamePattern.push(randomChosenColour);
+  $('#' + randomChosenColour)
+    .fadeOut(100)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+  playSound(randomChosenColour);
+  setTimeout(() => {
+    $('#level-title').text('Level ' + level);
+  }, 500);
 }
 
-$(".btn").on( "click", function() {
-    var userChosenColour = $(this).attr("id");
-    userClickedPattern.push(userChosenColour);
-    playSound(userChosenColour);
-    animatePress(userChosenColour);
-    console.log(userClickedPattern);    
-
-  } );
-
-  function animatePress(currentColour){
-    $("#"+currentColour).addClass("pressed");
-    setTimeout(function () {
-        $("#" + currentColour).removeClass("pressed");
-      }, 100);
+function checkAnswer(currentLevel) {
+  // console.log('user clicked: ' + userClickedPattern);
+  // console.log('game pattern ' + gamePattern);
+  var level = currentLevel - 1;
+  console.log(userClickedPattern[level]);
+  if (userClickedPattern[level] == gamePattern[level]) {
+    if (userClickedPattern.length == gamePattern.length) {
+      console.log('true');
+      setTimeout(() => {
+        nextSequence();
+      }, 1000);
+      userClickedPattern.length = 0;
+    }
+  } else {
+    console.log('false');
+    gameOver();
+    setTimeout(() => {
+      gameOver();
+    }, 300);
   }
+}
 
+$('.btn').on('click', function () {
+  var userChosenColour = $(this).attr('id');
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+  checkAnswer(userClickedPattern.length);
+});
 
-  function playSound(name) {
+function animatePress(currentColour) {
+  $('#' + currentColour).addClass('pressed');
+  setTimeout(function () {
+    $('#' + currentColour).removeClass('pressed');
+  }, 100);
+}
 
-    //3. Take the code we used to play sound in the nextSequence() function and add it to playSound().
-    var audio = new Audio("sounds/" + name + ".mp3");
-    audio.play();
+function playSound(name) {
+  //3. Take the code we used to play sound in the nextSequence() function and add it to playSound().
+  var audio = new Audio('sounds/' + name + '.mp3');
+  audio.play();
+}
+
+function gameOver() {
+  $('body').addClass('game-over');
+  setTimeout(() => {
+    $('body').removeClass('game-over');
+  }, 200);
+
+  var audio = new Audio('./sounds/wrong.mp3');
+  audio.play();
+
+  $('h1').text('Game Over, Press Any Key to Restart');
+
+  startOver();
+}
+
+function startOver() {
+  gamePattern = [];
+  userClickedPattern = [];
+  level = 0;
+  started = false;
+}
+
+function highScore() {
+  if (level >= highscore) {
+    highscore = level;
   }
+  $('.high-score').css('display', 'inline');
+  $('.high-score').text('High score ' + highscore);
+}
 
+$('.img').click(function () {
+  hint();
+});
 
+function hint() {
+  $('video').removeClass('none');
+  $('video').addClass('hint');
+  video();
+}
+function video() {
+  let vid = $('video');
+  vid.play();
+}
